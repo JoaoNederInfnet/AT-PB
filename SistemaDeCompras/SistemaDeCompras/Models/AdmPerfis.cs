800892afa663634c7fe8dfd730e0973572da54d9
João  Pedro Neder Resende de Lima <Jpnereli@Mac-mini.local>
2025-09-18 20:47:56 -0300

    0.5: Adicionando o corpo do model do Gestor e dos Administradores

--- SistemaDeCompras/SistemaDeCompras/Models/AdmPerfis.cs
+++ SistemaDeCompras/SistemaDeCompras/Models/AdmPerfis.cs
@@ -1,6 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SistemaDeCompras.Models;
 
 public class AdmPerfis : Model
 {
+    /*/ ------------------------------- PROPRIEDADES ------------------------------- /*/
+    //1) Nome
+    [Required(ErrorMessage = "O nome de usuário é obrigatório!")]
+    public string Nome { get; set; }
+    //--------------------------------------------/------------------------------------------
     
+    //2) Senha
+    [Required(ErrorMessage = "A senha é obrigatória!")]
+    public string Senha { get; set; }
+    //========================================================
+    
+    /*/ ------------------------------- CONSTRUTORES ------------------------------- /*/
+    //1) Cheio
+    public AdmPerfis(string nome, string senha)
+    {
+        Nome = nome;
+        Senha = senha;
+    }
+    //========================================================
 }
